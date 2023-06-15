30e50c1a9c7c83861fc67d4dac3570fc5d0ac6ae
KJ-HAN1:git config --global user.name KJ-HAN1git config --global user.name KJ-HAN1 <hkjin@kakao.com>
2023-06-16 02:27:01 +0900

    2023_06_15_add

--- greedy/camp.cpp
+++ greedy/camp.cpp
@@ -1,17 +1,21 @@
+/*
+2023_06
+backjoon 4796 - 캠핑  https://www.acmicpc.net/problem/4796
+*/
 #include<iostream>
 
 using namespace std;
 
 int main(){
 
-    int L,P,V;
+    int L,P,V;  // L= 사용가능 일수 P = 연속한 일수 V = 휴가
     int sum;
-    int Case = 1;
+    int Case = 1; 
 
     while(1){
         sum = 0;
         cin >> L >> P >> V;
-        if(L == 0 && P ==0 && V == 0) break;
+        if(L == 0 && P ==0 && V == 0) break;  // 0 0 0 입력시 종료
 
         sum += (V/P)*L;
         if(V%P > L) sum += L;
