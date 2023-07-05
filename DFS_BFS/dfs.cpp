f8a8927b99959bda314b6d41d574d1447320a21b
KJ-HAN1:git config --global user.name KJ-HAN1git config --global user.name KJ-HAN1 <hkjin@kakao.com>
2023-07-05 21:47:24 +0900

    2023_07_06

--- DFS_BFS/dfs.cpp
+++ DFS_BFS/dfs.cpp
@@ -1,45 +1,34 @@
-/* 2202_11_20
+/* 2023_06_26
 Han, KukJin
-backjoon dfs
+backjoon 24480 - dfs  https://www.acmicpc.net/problem/24480
+DFS 활용
 */
 #include<iostream>
-#define MAX 9999999
+#include<vector>
+#include<algorithm>
 using namespace std;
+
 int N,M,R;
-bool invited[MAX];
-int map[99999][999];
+vector<int> visited;
 
-void reset(){
-    for(int i=0;i<N;i++){
-        invited[i] = 0;
-    }
-}
-void dfs(int x){
-    invited[x] = true;
-    cout << x << endl;
 
-    for(int i=N; i>=1;i--){
-        if(map[x][i] == 1 & invited[i] == 0){
-            dfs(i);
-        } 
-    }
-    
+void DFS(int R){
+
 }
 
 int main(){
-    cin >> N >> M >> R;
-    for(int i=0;i<N;i++){
+    cin >> N>>M>>R;
+    vector<vector<int>> v(N);
+    for(int i=0;i<M;i++){
         int a,b;
-        cin >> a >> b;
-        map[a][b] = 1;
-        map[b][a] = 1;
+        cin >>a >>b;
+        v[a].push_back(b);
+        v[b].push_back(a);
     }
-    reset();
-    dfs(R);
-    for(int i=N; i>=1;i--){
-        if(map[R][i] == 0 & invited[i] == 0){
-            cout << "0" << endl;
-        } 
+    for(int i=0; i<N;i++){
+        sort(v[i].begin(), v[i].end(), greater<int>());
     }
+    DFS(R);
+
     return 0;
 }
